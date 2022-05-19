3dc87b96ab9781ec5393875ceb5fa1aa03328206
Luis Morillo <luis_cnnvd@hotmail.com>
2022-05-19 21:34:01 +0200

    make better module encapsulation

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,11 @@
+
+{-|
+This modules is entry point of the game. When a haskell executable is called, 
+it enters the main function (as in any other language). The main function must have
+type IO (), that is input/output returning nothing (). Because, in haskell we can't
+implement IO code with out monads, this module is implemented for you. 
+-}
+
 module Main where
 
 import qualified Board
@@ -8,41 +16,21 @@ import qualified Data.Sequence as S
 import System.Environment (getArgs)
 import Control.Concurrent
     ( forkIO, threadDelay )
-import System.IO (stdin, hReady, hSetBuffering, BufferMode (NoBuffering), hSetEcho)
+import System.IO (stdin, hSetBuffering, BufferMode (NoBuffering), hSetEcho)
 import Control.Concurrent.BoundedChan
-    ( newBoundedChan, tryReadChan, tryWriteChan )
-import EventQueue ( Event(..), EventQueue(EventQueue) )
+    ( newBoundedChan )
+import EventQueue ( Event(..), EventQueue(EventQueue), writeUserInput, readEvent )
 
 
--- EventQueue utils
-writeUserInput :: EventQueue -> IO ()
-writeUserInput queue@(EventQueue userqueue) = do
-    c <- getKey
-    case c of
-      "\ESC[A" -> tryWriteChan userqueue Snake.North >> writeUserInput queue
-      "\ESC[D" -> tryWriteChan userqueue Snake.West  >> writeUserInput queue
-      "\ESC[C" -> tryWriteChan userqueue Snake.East  >> writeUserInput queue
-      "\ESC[B" -> tryWriteChan userqueue Snake.South >> writeUserInput queue
-      _   -> writeUserInput queue
+-- This are auxiliar functions for game initialization. It produces two random points. 
 
-readEvent :: EventQueue -> IO Event
-readEvent (EventQueue userqueue) = do
-  mv <- tryReadChan userqueue
-  case mv of
-    Nothing   -> return Tick
-    Just move -> return $ UserEvent move
-
-
-getKey :: IO [Char]
-getKey = reverse <$> getKey' ""
-  where getKey' chars = do
-          char <- getChar
-          more <- hReady stdin
-          (if more then getKey' else return) (char:chars)
-
+-- | Given height and width, it produces a random point in the board.
 getRandomPoint :: Int -> Int -> IO Board.Point
 getRandomPoint h w = (,) <$> randomRIO (1, h) <*> randomRIO (1, w)
 
+-- | Given height and width, it produces two random points in the board
+-- one for the snake head and one for the apple. If makes sure, both aren't 
+-- the same point
 inititalizePoints :: Int -> Int -> IO (Board.Point, Board.Point)
 inititalizePoints h w = do
   (snakeInit, appleInit) <- (,) <$> getRandomPoint h w <*> getRandomPoint h w
@@ -50,14 +38,15 @@ inititalizePoints h w = do
     then inititalizePoints h w
     else return (snakeInit, appleInit)
 
-
-
+-- Entry point
 main :: IO ()
 main = do
     -- enable reading key strokes
     hSetBuffering stdin NoBuffering
     hSetEcho stdin False
-    -- Game Init
+
+    -- Game Initialization. This just set up the board and the initial positions of the 
+    -- snake and the apple.
     [h, w, timeSpeed] <- fmap read <$> getArgs
     (snakeInit, appleInit) <- inititalizePoints h w
     sg <- getStdGen
@@ -66,23 +55,26 @@ main = do
         board = Board.buildInitialBoard binf snakeInit appleInit
     newUserEventQueue <- newBoundedChan 3
     let eventQueue = EventQueue newUserEventQueue
+
+    -- Game execution. We execute and asyncronous proccess for recording key strokes in the
+    -- event queue and in the main thread we execute the game loop
     _ <- forkIO $ writeUserInput eventQueue
     gameloop gameState board timeSpeed eventQueue
 
   where
     gameloop :: Snake.AppState -> Board.RenderState -> Int -> EventQueue -> IO ()
-    gameloop app b timeSpeed queue = do
-        threadDelay timeSpeed
-        event <- readEvent queue
-        let (app',delta) =
+    gameloop app b timeSpeed queue = do 
+        threadDelay timeSpeed                                              -- Pause the loop for the timespeed we are running
+        event <- readEvent queue                                           -- Read the event from the queue
+        let (app',delta) =                                                 -- Calculate the new state and the messages to pass to the rendering state
               case event of
-                    Tick           -> Snake.move app
-                    UserEvent move ->
-                      if Snake.movement app == Snake.opositeMovement move
+                    Tick           -> Snake.move app                       -- This is the case no user input is in the queue. We just move
+                    UserEvent move ->                                      -- This is the case the user has move. We need to check if the user is not
+                      if Snake.movement app == Snake.opositeMovement move  -- moving against the current movement of the snake, and modify the movement accordingly.
                         then Snake.move app
                         else Snake.move $ app {Snake.movement = move}
-            board' = b `Board.updateRenderState` delta
-        putStr "\ESC[2J"
-        putStr $ Board.render board'
-        gameloop app' board' timeSpeed queue
+            board' = b `Board.updateRenderState` delta                     -- Update the render state
+        putStr "\ESC[2J"                                                   -- Clear the screen
+        putStr $ Board.render board'                                       -- print the board in the console.
+        gameloop app' board' timeSpeed queue                               -- run again the loop
 
--- src/EventQueue.hs
+++ src/EventQueue.hs
@@ -2,12 +2,17 @@
 
 {-|
 This module handle the external events of the game. That is: the user inputs and the time. 
+Is a good example about how to use a Concurrent Bounded Channel. Because we can't make concurrent
+code without monads, this module is implemented. Nevertheless is worth to read and to get a feeling
+on how monadic code looks.
 -}
+
 module EventQueue where
 
 import qualified Snake
 import Control.Concurrent.BoundedChan
-    ( BoundedChan )
+    ( BoundedChan, tryWriteChan, tryReadChan )
+import System.IO (hReady, stdin)
 
 
 -- | The are two kind of events, a `ClockEvent`, representing movement which is not force by the user input, and `UserEvent` which is the opposite.
@@ -19,9 +24,38 @@ type UserInputQueue = BoundedChan Snake.Movement
 -- | The `EventQueue` has a `TimeQueue` a `UserInputQueue` and the global speed of consumption. The speed is represented by the current speed
 newtype EventQueue = EventQueue {userInput :: UserInputQueue}
 
--- | Given the current score, updates the global shared speed every 10 points by a factor of 10%. Returns the current state
-calculateSpeed :: Int -> Int -> Int
-calculateSpeed score initialSpeed =
-    let level = min score 50 `quot` 10              -- maximun of 5 levels every 10 apples
-        speedFactor = 1 - fromIntegral level / 10.0 -- every level speeds up the time by a 10%
-     in floor @Double $ fromIntegral initialSpeed * speedFactor
+
+-- | This an auxiliry function to read key strokes from the terminal.
+getKey :: IO [Char]
+getKey = reverse <$> getKey' ""
+  where getKey' chars = do
+          char <- getChar
+          more <- hReady stdin
+          (if more then getKey' else return) (char:chars)
+
+-- --------------------------------
+-- * Interface for the EventQueue * 
+-- --------------------------------
+
+-- | Writes Movement based on the key pressed. As implemented, the arrow keys. 
+--   It loops forever.
+writeUserInput :: EventQueue -> IO ()
+writeUserInput queue@(EventQueue userqueue) = do
+    c <- getKey
+    case c of
+      "\ESC[A" -> tryWriteChan userqueue Snake.North >> writeUserInput queue
+      "\ESC[D" -> tryWriteChan userqueue Snake.West  >> writeUserInput queue
+      "\ESC[C" -> tryWriteChan userqueue Snake.East  >> writeUserInput queue
+      "\ESC[B" -> tryWriteChan userqueue Snake.South >> writeUserInput queue
+      _   -> writeUserInput queue
+
+-- | Reads the EventQueue, if it is empty, returns a Tick, else returns, the Movement
+-- store in the queue
+readEvent :: EventQueue -> IO Event
+readEvent (EventQueue userqueue) = do
+  mv <- tryReadChan userqueue
+  case mv of
+    Nothing   -> return Tick
+    Just move -> return $ UserEvent move
+
+
