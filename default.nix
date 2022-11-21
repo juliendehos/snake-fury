7a18385f89b1d6c2c892bc6acb5732e4f369fb44
Julien Dehos <dehos@univ-littoral.fr>
2022-11-21 19:33:22 +0100

    nix config ok

---
+++ default.nix
@@ -0,0 +1,12 @@
+let
+  pkgs = import <nixpkgs> { };
+  compiler = pkgs.haskellPackages;
+
+in compiler.developPackage {
+  root = ./.;
+  modifier = drv:
+  pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages; [
+    cabal-install
+  ]);
+}
+
